08697df22e197e55e2da1a18ab60f274eed29655
dohessiekan <d.gnondoyi@alustudent.com>
2023-02-06 15:31:28 -0800

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # 6-print_comb3.py
-for x in range(0, 10):
-    for y range(1, 10):
+for x in range(0,10):
+    for y range(1,10):
         if x >= y:
             continue
-        elif x == 8 and y == 0
+        elif x == 8 and y == 9:
             print("{}{}".format(x,y))
         else:
             print("{}{}".format(x,y), end=", ")
