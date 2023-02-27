164e4d52c3dd0ea23438a66117e6365bf338ac97
dohessiekan <d.gnondoyi@alustudent.com>
2023-02-27 02:07:05 -0800

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,8 @@
-vi 0-add.py
-
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+from add_0 import add
 
-    a = 1
-    b = 2
+a = 1
+b = 2
 
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if __name__ == '__main__':
+    print("{} + {} = {}".format(a, b, add(a, b)))
